fc679a48602b1ebf929924968cd0851d8070a1bc
tp1050 <tp1050@gmail.com>
2022-04-17 22:56:16 +0430

    m

--- app/__init__.py
+++ app/__init__.py
@@ -18,8 +18,8 @@ inventory = {
 }
 
 app.config.from_object(Conf)
-from views import InventoryApi,InventoryItemApi
-
-app.add_url_rule("/api/inventory",view_func=InventoryApi.as_view("inventory_api"))
-app.add_url_rule("/api/inventory/<item_name>",view_func=InventoryItemApi.as_view("inventory_item_api"))
+# from views import InventoryApi,InventoryItemApi
+#
+# app.add_url_rule("/api/inventory",view_func=InventoryApi.as_view("inventory_api"))
+# app.add_url_rule("/api/inventory/<item_name>",view_func=InventoryItemApi.as_view("inventory_item_api"))
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import jsonify
 from app import logs
 from flask import render_template
 from flask import request
@@ -9,36 +10,45 @@ from werkzeug.exceptions import HTTPException
 
 #Defining default for route leads to closed routing system
 # and allows for totall control of incoming requests.
-@app.route('/', defaults={'path': ''},methods=['POST','GET'])
-@app.route('/<path:path>')
-def default(path):
-    return f"{path} is kosher"
+# @app.route('/', defaults={'path': ''},methods=['POST','GET'])
+# @app.route('/<path:path>')
+# def default(path):
+#     return f"{path} is kosher"
 
 
 
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    # start with the correct headers and status code from the error
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
+# @app.errorhandler(HTTPException)
+# def handle_exception(e):
+#     """Return JSON instead of HTML for HTTP errors."""
+#     # start with the correct headers and status code from the error
+#     response = e.get_response()
+#     # replace the body with JSON
+#     response.data = json.dumps({
+#         "code": e.code,
+#         "name": e.name,
+#         "description": e.description,
+#     })
+#     response.content_type = "application/json"
+#     return response
+# #
+# #
+# # @app.before_request()
+# # def before_request():
+# #     print(request.form)
 
+# @app.before_request
+# def before_request():
+#     print('before')
+#     # print(request.get_data())
+#     print('beforish')
 
-@app.before_request()
-def before():
-    print(request.form)
+@app.route('/echoJSON', methods=['POST'])
+def echoJSON():
+    data = request.json
+    return jsonify(data)
 
-
-
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     ip = 'Unknown Visitor'
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
@@ -46,6 +56,9 @@ def index():
     else:
         ip = request.environ['HTTP_X_FORWARDED_FOR'] # if behind a proxy
     logs.append(ip)
+    print('index')
+    print(request.environ.items())
+    print('indexed')
     return render_template("index.html", title=str(logs),user={'name':ip},ip=ip)
 
 @app.route('/addProduct')
